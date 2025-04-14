a78505d26552e08f493ac92e38565f8f15612ef8
Jackhammer88 <jackhammer88@users.noreply.github.com>
2025-04-14 16:38:58 +0300

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,19 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
+const dev = process.env.NODE_ENV !== 'production';
+
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html',
-			base: ''
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/pltrends'
+		}
 	}
 };
 
